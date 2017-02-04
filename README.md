a574551f411f57c99cf7f4fa8594fcf890560c0a
Alexey Kovrizhkin <jean@tender.pro>
2017-02-04 21:58:11 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+
+apitools - set of tools for [dbrpc](https://github.com/LeKovr/dbrpc)
+
+* [generate .md with methods definition](doc_gen.sh)
+* [generate .md with api usage script log](apiscript.sh)
+* [api methos call benchmarks](bench.sh)
---
+++ apiscript.sh
@@ -0,0 +1,229 @@
+#!/bin/bash
+#
+# apiscript.sh v1.1
+# Генерация .md с описанием вызовов методов API по сценарию
+# Оригинальная версия файла: https://github.com/LeKovr/dbrpc/blob/master/doc_gen.sh
+# (c) 2017 Tender.Pro, Алексей Коврижкин, jean@tender.pro
+#
+# Use:
+# -- local docs
+# bash doc_gen.sh samples.sc > samples.md
+#
+
+# -------------------------------------------------------------------------------
+
+SCRIPT=$1
+shift
+
+[[ "$SCRIPT" ]] || {
+  echo "usage: $0 <script>"
+  exit
+}
+
+# Суффикс добавляется к алиасам пользователей и компаний для уникальности
+KEY=$1
+[[ "$KEY" ]] || KEY=""
+
+
+# TOKEN берем из БД: select * from wsd.auth_token;
+#TOKEN=55d79928-911a-4d4e-a6f3-4e2a377fcbba
+
+# хост сервера АПИ
+#APP_SITE="30502.zen.lan"
+
+. .config
+
+# Непустое значение включает отладочный вывод
+DEBUG=""
+
+
+# -------------------------------------------------------------------------------
+
+declare -A DATA
+API=http://$APP_SITE/rpc
+CTYPE="Content-type: application/json"
+
+# -------------------------------------------------------------------------------
+
+. $SCRIPT
+
+# -------------------------------------------------------------------------------
+
+toc() {
+  local new_key=$1
+  shift
+  if [[ "$new_key" == "=" ]] ; then
+    local code=$1
+    shift
+    echo "* $start. [$code](#${start}-$code) $@"
+    (( start++ ))
+
+  fi
+}
+
+# -------------------------------------------------------------------------------
+
+parse_token() {
+  local key=$1
+
+  # trim suffix
+  local t12=${key%.*}
+
+  # trim prefix
+  local t2=${t12#*.}
+
+  [[ "$t2" == "null" ]] && {
+    >&2 echo "Error: empty token"
+    exit
+  }
+  # TODO: base64 writes to STDERR
+  echo -n "$t2" | base64 --d 2>/dev/null | jq .
+}
+
+# -------------------------------------------------------------------------------
+
+process() {
+  local new_key=$1
+  shift
+  local use_key=$1
+  shift
+
+  if [[ "$new_key" == "=" ]] ; then
+    # заголовок
+    echo -e "\n## $start. $use_key\n\n$@\n"
+    (( start++ ))
+    return
+  elif [[ "$new_key" == "." ]] ; then
+    # вывести текст без изменений
+    echo $use_key $@
+    return
+  elif [[ "$new_key" == ".sleep" ]] ; then
+    # сделать паузу
+    >&2 echo "sleep $use_key"
+    sleep $use_key
+    return
+  fi
+
+  local method=$1
+  shift
+  local args=$@
+
+  # парсим первый аргкмент команды
+  # формат: XX{;XX}, где XX может иметь видa
+  # AA - из хэша результата поле .token поместить в переменную AA
+  # +AA - в строке аргументов заменить строку =AA= на значение переменной AA
+  # AA=BB - из массива результата взять  первый хэш и из него поле .BB поместить в переменную AA
+
+  OIFS=$IFS
+  IFS=';'
+  vars=$new_key
+  new_key="-"
+  for x in $vars
+  do
+    [[ "$DEBUG" ]] && echo "arg1: $x"
+    if [[ "$x" == +* ]] ; then
+      # переменная в аргументах
+      # trim prefix
+      local name=${x#+}
+      args="${args/=$name=/${DATA[$name]}}"
+      [[ "$DEBUG" ]] && echo ">> $name -> $args"
+    else
+      new_key=$x
+    fi
+  done
+
+  IFS=$OIFS
+
+  local auth=""
+  if [[ "$use_key" != "-" ]] ; then
+    # если задан второй аргумент - использовать значение этой переменной как токен авторизации
+    [[ "$DEBUG" ]] && echo ">> Use key: $use_key - ${DATA[$use_key]}"
+    auth="-H \"Authorization: Bearer xxxx.xxxx.xxxx\""
+    resp=$(curl -sd "$args" -H "Authorization: Bearer ${DATA[$use_key]}" -H "$CTYPE" $API/$method)
+  else
+    # если не задан - запрос без авторизации
+    resp=$(curl -sd "$args" $auth -H "$CTYPE" $API/$method)
+  fi
+
+cat <<EOF
+
+\`\`\`
+CALL=$API/$method
+Q='$args'
+curl -gsd "\$Q" -H "Content-type: application/json" $auth \$CALL | jq '.'
+\`\`\`
+EOF
+
+    [[ "$DEBUG" ]] && echo "$resp"
+    result=$(echo "$resp" | jq '.')
+
+  if [[ "$new_key" == *=* ]] ; then
+    # забрать в массив элемент из хэша результата
+    # trim suffix
+    local dest=${new_key%=*}
+    # trim prefix
+    local src=${new_key#*=}
+    DATA[$dest]=$(echo "$resp" | jq -r ".[] | .$src")
+    [[ "$DEBUG" ]] && echo ">> $dest = $src (${DATA[$dest]})"
+    new_key="-"
+  fi
+
+  if [[ "$new_key" != "-" ]] ; then
+    # забрать в массив токен
+    DATA[$new_key]=$(echo $resp| jq -r '.token')
+    [[ "$DEBUG" ]] && echo ">> Save key: $new_key - ${DATA[$new_key]}"
+
+cat <<EOF
+\`\`\`json
+{
+  "token": "xxxx.xxxx.xxxx"
+}
+\`\`\`
+
+Token payload:
+\`\`\`json
+EOF
+
+parse_token ${DATA[$new_key]}
+
+echo "\`\`\`"
+else
+cat <<EOF
+\`\`\`json
+$result
+\`\`\`
+
+EOF
+
+  fi
+
+
+}
+# -------------------------------------------------------------------------------
+
+run() {
+
+  echo -e "\n# $TITLE\n\n"
+
+  local start=1
+  while IFS= read -r line ; do
+    # Skip comments
+    q=${line%%#*} # remove endline comments
+    [ -n "${q##+([[:space:]])}" ] || continue # ignore line if contains only spaces
+
+    toc $q
+  done <<< "$Q"
+  start=1
+
+  while IFS= read -r line ; do
+    # Skip comments
+    q=${line%%#*} # remove endline comments
+    [ -n "${q##+([[:space:]])}" ] || continue # ignore line if contains only spaces
+
+    process $q # >> $OUT
+  done <<< "$Q"
+}
+
+# -------------------------------------------------------------------------------
+
+run
